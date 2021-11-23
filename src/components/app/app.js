c203fbb2aec9e494bc5b5feafa2741c62d384180
Maxim <maxianikin@gmail.com>
2021-11-23 23:33:31 +0300

    feat: add form state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,6 @@
 import AppFilter from '../app-filter/app-filter';
 import AppInfo from '../app-info/app-info';
-import EmployeesAddForm from '../employers-add-form/employers-add-form';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
 import EmployersList from '../employers-list/employers-list';
 import SearchPanel from '../search-panel/search-panel';
 import './app.css'
@@ -21,7 +21,7 @@ function App() {
                 <AppFilter/>
             </div>
             <EmployersList data={data}/>
-            <EmployeesAddForm/>
+            <EmployersAddForm/>
         </div>
     )
 }
