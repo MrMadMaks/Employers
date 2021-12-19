ddd755440d78e7d6d3fd62a0c996476f991da4dd
Maxim <maxianikin@gmail.com>
2021-12-19 17:48:28 +0300

    feat: finished app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,10 +14,12 @@ class App extends Component {
         super(props);
         this.state = {
             data: [
-                { name: 'Максим Иванов', salary: 300, increase: false, id: 1 },
-                { name: 'Иван Петров', salary: 500, increase: true, id: 2 },
-                { name: 'Алексей Королёв', salary: 7000, increase: false, id: 3 },
-            ]
+                { name: 'Максим Иванов', salary: 300, increase: false,  star: true, id: 1 },
+                { name: 'Иван Петров', salary: 500, increase: true, star: false, id: 2 },
+                { name: 'Алексей Королёв', salary: 7000, increase: false, star: false, id: 3 },
+            ],
+            term: '',
+            filter: 'all'
         }
         this.maxId = 4
     }
@@ -34,26 +36,76 @@ class App extends Component {
         const newItem = {
             name,
             salary,
+            star: false,
             increase: false,
             id: this.maxId++
         }
         this.setState(({ data }) => {
             const newArr = [...data, newItem];
-            return {
-                data: newArr
+            if (name.length > 3) {
+                return {
+                    data: newArr
+                }
             }
         });
     }
 
+    onToggleProp = (id, prop) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if(item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item
+            })
+        }))
+    }
+
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1;
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term})
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.star);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items;
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
+
     render() {
+        const {data, term, filter} = this.state;
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
+
         return (
             <div className="app" >
-                <AppInfo />
+                <AppInfo employees={employees} increased={increased}/>
                 <div className="search-panel">
-                    <SearchPanel />
-                    <AppFilter />
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter}
+                               onFilterSelect={this.onFilterSelect}/>
                 </div>
-                <EmployersList data={this.state.data} onDelete={this.deleteItem} />
+                <EmployersList data={visibleData}
+                               onDelete={this.deleteItem}
+                               onToggleProp={this.onToggleProp} />
                 <EmployersAddForm onAdd={this.addItem} />
             </div>
         )
--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -1,32 +1,8 @@
-import { Component } from 'react';
 import './employers-list-item.css'
 
-class EmployersListItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            increase: false,
-            star: false
-        }
+const EmployersListItem = (props) => {
 
-    }
-
-    onIncrease = () => {
-        this.setState(({ increase }) => ({
-            increase: !increase
-        }))
-    }
-
-    star = () => {
-        this.setState(({ star }) => ({
-            star: !star
-        }))
-    }
-
-    render() {
-
-        const { name, salary, onDelete } = this.props
-        const { increase, star } = this.state
+        const { name, salary, onDelete, onToggleProp, increase, star } = props
 
         let classNames = "list-group-item d-flex justify-content-between";
         if (increase) {
@@ -38,12 +14,13 @@ class EmployersListItem extends Component {
 
         return (
             <li className={classNames}>
-                <span className="list-group-item-label" onClick={this.star}>{name}</span>
+                <span className="list-group-item-label" onClick={onToggleProp} data-toggle='star'>{name}</span>
                 <input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
                 <div className='d-flex justify-content-center align-items-center'>
                     <button type="button"
                         className="btn-cookie btn-sm "
-                        onClick={this.onIncrease}>
+                        onClick={onToggleProp}
+                            data-toggle='increase'>
                         <i className="fas fa-cookie"></i>
                     </button>
 
@@ -56,7 +33,6 @@ class EmployersListItem extends Component {
                 </div>
             </li>
         )
-    }
 }
 
 export default EmployersListItem;
